40416ab79b0206c8dc9d5373c64aaaec53ae3ff2
Daniel Schultz <slifty@gmail.com>
2012-09-27 13:12:29 -0400

    added documentation

--- game/classes.js
+++ game/classes.js
@@ -147,7 +147,7 @@ exports.Interaction = function() {
 	this.id = uuid.v4();
 	this.isSsl = false;
 	this.isTor = false;
-	this.message = null;
-	this.responses = [];
-	this.socket = null;
+	this.message = null; // The message that initiated this interaction
+	this.responses = []; // The messages that have been sent as a response to this interaction
+	this.socket = null; // The socket that initiated this interaction
 }
