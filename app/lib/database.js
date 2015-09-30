1ed09bba5fddd8c281cb3c5bcbcf4855a74be15e
Marc Gunn <telrikk@gmail.com>
2015-09-30 01:00:28 -0700

    wait for callback before imports, just to be safe

--- app/lib/database.js
+++ app/lib/database.js
@@ -1,5 +1,6 @@
 var logger = require('./logger').logger
 var config = require('../../config')
+var chalk = require('chalk')
 
 if(!global.hasOwnProperty('database')) {
   var Sequelize = require('sequelize');
@@ -21,25 +22,25 @@ if(!global.hasOwnProperty('database')) {
       }
     });
 
-  sequelize
-    .authenticate()
-    .then(function(err) {
-      logger.info('Connection has been established successfully.');
-    }, function (err) { 
-      logger.error('Unable to connect to the database:', err);
-    });
-
   global.database = {
     sequelize: sequelize
   };
 
-  var modelPath = __dirname + "/../models";
-  fs.readdirSync(modelPath).forEach(function(file) {
-    var name = file.substr(0, file.indexOf('.'));
-    global.database[name] = sequelize.import(modelPath + "/" + name);
-  });
+  sequelize
+    .authenticate()
+    .then(function() {
+      logger.info('Connection has been established successfully.');
+      var modelPath = __dirname + "/../models";
+      fs.readdirSync(modelPath).forEach(function(file) {
+        var name = file.substr(0, file.indexOf('.'));
+        logger.info(chalk.green('Loading model ' + name));
+        global.database[name] = sequelize.import(modelPath + "/" + name);
+      });
 
-  sequelize.sync();
+      sequelize.sync();
+    }, function (err) { 
+      logger.error('Unable to connect to the database:', err);
+    });
 }
 
 module.exports = global.database
