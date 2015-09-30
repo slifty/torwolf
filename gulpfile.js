701b367e895778afcd677532d5cd29a94c6c1b4a
Marc Gunn <telrikk@gmail.com>
2015-09-30 02:08:37 -0700

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var mocha = require('gulp-mocha');
+var coverage = require('gulp-coverage');
+var gulp = require('gulp');
+
+gulp.task('test', function () {
+    return gulp.src('test/**/*Test.js')
+        .pipe(mocha())
+        .once('error', function () {
+            process.exit(1);
+        })
+        .once('end', function () {
+            process.exit();
+        });
+});
+
+gulp.task('coverage', function () {
+    return gulp.src('test/**/*Test.js')
+	    .pipe(coverage.instrument({
+	        pattern: ['app/**/*.js'],
+	        debugDirectory: 'debug'
+	    }))
+	    .pipe(mocha())
+	    .pipe(coverage.gather())
+	    .pipe(coverage.format())
+	    .pipe(gulp.dest('reports'));
+});
